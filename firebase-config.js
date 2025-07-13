194a4d43a70ab209a4dea51d6f98aea05ab6a96b
CBMSA <153248452+CBMSA@users.noreply.github.com>
2025-07-13 07:08:19 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-FIREBASE-AUTHDOMAIN",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-BUCKET",
+  messagingSenderId: "SENDER-ID",
+  appId: "APP-ID"
+};
+firebase.initializeApp(firebaseConfig);
